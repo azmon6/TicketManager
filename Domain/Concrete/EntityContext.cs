0a3742b3704341e0dac483ca8c38265e4d43d2a5
kaloqn todorkov <kaloqn_bss@abv.bg>
2020-11-10 15:43:59 +0200

    Transfer to laptop

--- Domain/Concrete/EntityContext.cs
+++ Domain/Concrete/EntityContext.cs
@@ -1,11 +1,19 @@
 ﻿using TicketManager.Domain.Entities;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System;
+using System.Diagnostics;
 
 namespace TicketManager.Domain.Concrete
 {
     public class EntityContext : DbContext
     {
         public DbSet<Ticket> Tickets { get; set; }
+
+        public EntityContext()
+        {
+            //TODO Output SQL to output that is not Debug
+            this.Database.Log = sql => Trace.WriteLine(sql);
+        }
     }
 }
--- Domain/Entities/Ticket.cs
+++ Domain/Entities/Ticket.cs
@@ -1,22 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Web.Mvc;
 
 namespace TicketManager.Domain.Entities
 {
     public class Ticket
     {
+        //TODO ADD required but not with errormessage
         public int TicketID { get; set; }
+
+        [Required(ErrorMessage = "Enter a Ticket name")]
         public string TicketName { get; set; }
 
-        [Required(ErrorMessage = "Please enter a description")]
+        [Required(ErrorMessage = "Enter a Description")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Enter a Organizer")]
         public string Organizer { get; set; }
+        [Required(ErrorMessage = "Enter a Price")]
         public double Price { get; set; }
 
         //TODO Make Dates from string to DateTime
         //Use Jquerry for date picker
+        [Required(ErrorMessage = "Enter a Start of buying date")]
         public string StartBuyTime { get; set; }
+        [Required(ErrorMessage = "Enter a End of bying date")]
         public string EndBuyTime { get; set; }
+        [Required(ErrorMessage = "Enter a Event start time")]
         public string EventTime { get; set; }
     }
 }
